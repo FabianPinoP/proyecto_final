f7d9faa15626d8a26e11c428763b692ad1108c17
Juan Pablo <juanpablogast@gmail.com>
2021-08-03 22:12:21 -0400

    add root path

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   resources :parkings
   devise_for :users
+
+  root 'parkings#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
