37a1bac836ffd2f02efb260dae8ed3cfa611d77e
Juan Pablo <juanpablogast@gmail.com>
2021-08-05 19:33:02 -0400

    add autheticate user

--- app/controllers/parkings_controller.rb
+++ app/controllers/parkings_controller.rb
@@ -1,4 +1,5 @@
 class ParkingsController < ApplicationController
+  before_action :authenticate_user!
   before_action :set_parking, only: %i[ show edit update destroy ]
 
   # GET /parkings or /parkings.json
