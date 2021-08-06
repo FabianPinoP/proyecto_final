06e1a0edfd173e727399bd2886b4bd0637378f11
Juan Pablo <juanpablogast@gmail.com>
2021-08-06 17:29:46 -0400

    problem in modal

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -4,25 +4,54 @@ class Users::RegistrationsController < Devise::RegistrationsController
   before_action :configure_sign_up_params, only: [:create]
   before_action :configure_account_update_params, only: [:update]
 
+
+ 
+
+    def create
+      super
+    end
+  
+    def update
+      # For Rails 4
+      account_update_params = devise_parameter_sanitizer.sanitize(:account_update)
+  
+      # required for settings form to submit when password is left blank
+      if account_update_params[:password].blank?
+        account_update_params.delete("password")
+        account_update_params.delete("password_confirmation")
+      end
+  
+      @user = User.find(current_user.id)
+  
+      @update = update_resource(@user, account_update_params)
+      if @update 
+        # Sign in the user bypassing validation in case their password changed
+        sign_in @user, :bypass => true
+      end
+  
+      respond_to do |format|
+        format.html
+        format.js
+      end
+    end
+
+  
+
+
   # GET /resource/sign_up
   # def new
   #   super
   # end
 
-  # POST /resource
-  # def create
-  #   super
-  # end
+
 
   # GET /resource/edit
   # def edit
   #   super
   # end
 
-  # PUT /resource
-  # def update
-  #   super
-  # end
+
+
 
   # DELETE /resource
   # def destroy
