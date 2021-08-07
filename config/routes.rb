31b54efd97b86ebea623815ffed03ec6ef590bca
Juan Pablo <juanpablogast@gmail.com>
2021-08-06 20:38:30 -0400

    style red hand

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,7 @@ Rails.application.routes.draw do
   }
   delete '/users/:id', to: 'dashboards#destroy', as: :destroy_user
   get 'users/:id', to: 'dashboards#update_status', as: :update_status
+  
 
   get 'dashboard/', to: 'dashboards#index'
   get 'public_parkings/', to: 'dashboards#parking'
