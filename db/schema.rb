309e16b733596a98a402a1dc4bb3687b98ab8531
Juan Pablo <juanpablogast@gmail.com>
2021-08-07 20:25:15 -0400

    delete table

--- db/schema.rb
+++ db/schema.rb
@@ -10,29 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_07_232118) do
+ActiveRecord::Schema.define(version: 2021_08_07_231349) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "order_items", force: :cascade do |t|
-    t.bigint "orders_id"
-    t.bigint "parkings_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["orders_id"], name: "index_order_items_on_orders_id"
-    t.index ["parkings_id"], name: "index_order_items_on_parkings_id"
-  end
-
-  create_table "orders", force: :cascade do |t|
-    t.string "number"
-    t.string "total"
-    t.string "state"
-    t.bigint "user_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["user_id"], name: "index_orders_on_user_id"
-  end
 
   create_table "parkings", force: :cascade do |t|
     t.string "address"
@@ -90,8 +71,5 @@ ActiveRecord::Schema.define(version: 2021_08_07_232118) do
     t.index ["voter_id", "voter_type", "vote_scope"], name: "index_votes_on_voter_id_and_voter_type_and_vote_scope"
   end
 
-  add_foreign_key "order_items", "orders", column: "orders_id"
-  add_foreign_key "order_items", "parkings", column: "parkings_id"
-  add_foreign_key "orders", "users"
   add_foreign_key "parkings", "users"
 end
