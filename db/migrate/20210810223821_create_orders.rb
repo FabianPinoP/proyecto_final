4174e9dda09ff9853e91b36d33a3cda0308b9459
Fabian <fpinoperez@outlook.cl>
2021-08-10 20:08:41 -0400

    change in view shor cart and nabvar

--- db/migrate/20210810223821_create_orders.rb
+++ db/migrate/20210810223821_create_orders.rb
@@ -1,12 +1,12 @@
 class CreateOrders < ActiveRecord::Migration[5.2]
-  def change
-    create_table :orders do |t|
-      t.references :user, foreign_key: true
-      t.string :number
-      t.integer :total
-      t.string :state
-
-      t.timestamps
+    def change
+      create_table :orders do |t|
+        t.references :user, foreign_key: true
+        t.string :number
+        t.integer :total
+        t.string :state
+  
+        t.timestamps
+      end
     end
   end
-end
