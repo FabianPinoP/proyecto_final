bd139095c808f43934d9cb4c4777283b58010415
Fabian <fpinoperez@outlook.cl>
2021-08-12 22:54:13 -0400

    integration with mercado pago done

--- app/models/order.rb
+++ app/models/order.rb
@@ -3,21 +3,42 @@ class Order < ApplicationRecord
   validates :number, uniqueness: true
   has_many :order_items
   has_many :parkings, through: :order_items
+  before_create -> { generate_number(hash_size) }
 
-  def generate_number
-
-    self.number = loop do
-    rand_str = "PO#{Array.new(9){rand(9)}.split}"
-    break rand_str unless Order.exists?(number: rand_str)
-     end 
+  def generate_number(size)
+    self.number ||= loop do
+      random = random_candidate(size)
+      break random unless self.class.exists?(number: random)
+    end
   end
+
+  def random_candidate(size)
+    "#{hash_prefix}#{Array.new(size){rand(size)}.join}"
+  end
+
+  def hash_prefix
+    "BO"
+  end
+
+  def hash_size
+    9
+  end
+
 
   def add_parking(parking_id)
     parking = Parking.find(parking_id)
     if parking
     #creamos la orden con los datos del productos según queramos mostar
       order_items.create(parking_id: parking.id, price: parking.price)
+      price_total
     end
   end
 
+  def price_total
+    sum = 0
+    order_items.each do |item|
+      sum += item.price
+    end
+    update_attribute(:total, sum)
+  end
 end
